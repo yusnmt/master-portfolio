41b14b7d6ae8b75f3c3a38b1ef75af99e352c27d
yusnmt <yusnmt@gmail.com>
2022-02-24 18:00:15 +0900

    design completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import './navbar.css'
 const Menu = () => (
   <>
   <p><a href='#home'>Home</a></p>
-  <p><a href='#wgpt3'>What is GPT?</a></p>
+  <p><a href='#whgpt3'>What is GPT?</a></p>
   <p><a href='#possibility'>Open AI</a></p>
   <p><a href='#features'>Case studies</a></p>
   <p><a href='#blog'>Library</a></p>
@@ -20,7 +20,7 @@ const Navbar = () => {
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <img src={logo} />
+          <img src={logo} alt='logo'/>
         </div>
         <div className="gpt3__navbar-links_container">
           <Menu />
